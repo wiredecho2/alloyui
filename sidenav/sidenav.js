f27f7c3325e3deabbe51e6938a8f3676eeef1d7c
Philip Lee <philip.lee@liferay.com>
2016-12-15 12:39:16 -0800

    working sidenav

--- sidenav/sidenav.js
+++ sidenav/sidenav.js
@@ -1,38 +1,24 @@
 YUI().use(
-	'aui-node',
+	'aui-node', 'event',
 	function(Y) {
 		var node = Y.one('#toggleNav');
-		var current = true;
+			toggleBtn = Y.one('#toggleBtn');
+			current = true;
 
-		node.on(
+		toggleBtn.on(
 			'click',
 			function() {
 				if (current == true){
 					node.setStyle('width', 5);
-					node.setStyle('transition', 'width 0.2s ease-in-out');
+					node.setStyle('transition', 'width 0.1s ease-in-out');
 					node.setStyle('overflow-x', 'hidden');
 					current = false;
 				}else{
 					node.setStyle('width', 250);
+					node.setStyle('transition', 'width 0.1s ease-in-out');
 					current = true;
-					node.setStyle('transition', 'width 0.2s ease-in-out');
-
 				}
 			}
 		);
-	}
-);
-
-
-YUI().use(
-	'aui-button',
-	function(Y) {
-		new Y.ToggleButton(
-			{
-				label: 'Click to Toggle',
-				srcNode: '#toggleNav',
-				boundingBox: '#myToggleButton'
-			}
-		).render();
 	}
 );
